f88ce6594d306bf802b572f40a728c74e4a164e0
SouthpawCurveConcepts <jagoblacksky@gmail.com>
2022-03-16 11:28:40 -0500

    1class component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ClassComponent from "./Components/class-component.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>React Class Components</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/SouthpawCurveConcepts/rsb-class"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link to repo
         </a>
+        <ClassComponent />
       </header>
     </div>
   );
---
+++ src/Components/class-component.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class ClassComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>React Class Components</h2>
+        <p>
+          When creating a React component, the component's name must start with
+          an upper case letter.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default ClassComponent;
